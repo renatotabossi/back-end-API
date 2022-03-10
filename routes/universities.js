83381e0c3086b8362e0b2f65728d93236e4d0a37
Renato Tabossi <renatotabossi@gmail.com>
2022-03-10 20:59:32 -0300

    delete - patch - get by id - testes

--- routes/universities.js
+++ routes/universities.js
@@ -13,8 +13,13 @@ router.get("/", async (req, res) => {
 });
 
 // Get uma uni usando ID
-router.get("/:id", (req, res) => {
-    res.send(req.params.id);
+router.get("/:id", getUniversity, (req, res) => {
+    res.send({
+        ID: res.university.id,
+        NAME: res.university.name,
+        COUNTRY: res.university.country,
+        STATE_PROVINCE: res.university.state_province,
+    });
 });
 
 // Post uma uni
@@ -37,9 +42,46 @@ router.post("/", async (req, res) => {
 });
 
 // Update uma uni
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", getUniversity, async (req, res) => {
+    if (req.body.web_pages != null) {
+        res.university.web_pages = req.body.web_pages;
+    }
+    if (req.body.name != null) {
+        res.university.name = req.body.name;
+    }
+    if (req.body.domains != null) {
+        res.university.domains = req.body.domains;
+    }
+    try {
+        const updatedUniversity = await res.university.save();
+        res.json(updatedUniversity);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
 
 // Delete uma uni
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", getUniversity, async (req, res) => {
+    try {
+        await res.university.remove();
+        res.json({ message: "Deleted University" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+async function getUniversity(req, res, next) {
+    let university;
+    try {
+        university = await University.findById(req.params.id);
+        if (university == null) {
+            return res.status(404).json({ message: "Cannot find University" });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+    res.university = university;
+    next();
+}
 
 module.exports = router;
